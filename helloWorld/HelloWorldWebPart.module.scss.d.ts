5494c62ec6d5add2953dcad0e7cab3b178eaf6be
GOGREENGUY <110044161+GOGREENGUY@users.noreply.github.com>
2022-07-26 17:30:50 +0300

    Add files via upload

---
+++ helloWorld/HelloWorldWebPart.module.scss.d.ts
@@ -0,0 +1,11 @@
+declare const styles: {
+    helloWorld: string;
+    teams: string;
+    welcome: string;
+    welcomeImage: string;
+    links: string;
+    alertState: string;
+    value: string;
+};
+export default styles;
+//# sourceMappingURL=HelloWorldWebPart.module.scss.d.ts.map
---
+++ helloWorld/HelloWorldWebPart.module.scss.js
@@ -0,0 +1,14 @@
+/* tslint:disable */
+require("./HelloWorldWebPart.module.css");
+var styles = {
+    helloWorld: 'helloWorld_b556f492',
+    teams: 'teams_b556f492',
+    welcome: 'welcome_b556f492',
+    welcomeImage: 'welcomeImage_b556f492',
+    links: 'links_b556f492',
+    alertState: 'alertState_b556f492',
+    value: 'value_b556f492'
+};
+export default styles;
+/* tslint:enable */ 
+//# sourceMappingURL=HelloWorldWebPart.module.scss.js.map
